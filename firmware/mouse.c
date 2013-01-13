577646ca3746e5b32e0448da21b5cd8f6866ad81
vinodstanur <vinodstanur@gmail.com>
2013-01-13 17:02:13 +0530

    c indent

--- firmware/mouse.c
+++ firmware/mouse.c
@@ -1,3 +1,28 @@
+/*
+Touchscreen based bluetooth mouse
+
+microcontroller: ATTiny13
+author: Vinod S
+email:  vinodstanur at gmail dot com
+date:   12/1/2013
+homepage:    http://blog.vinu.co.in
+ 
+compiler: avr-gcc
+Copyright (C) <2013>  <http://blog.vinu.co.in>
+ 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+ 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <avr/io.h>
 #define F_CPU 9600000
 #include <util/delay.h>
@@ -6,54 +31,54 @@
 unsigned char data;
 volatile char trigger;
 
-void adc_init() {
-		DIDR0 |= 1<<ADC2D;
-		ADMUX |= 1<<MUX1;
-		ADCSRA |= (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
-		ADCSRA |= (1<<ADEN);
+void adc_init()
+{
+	DIDR0 |= 1 << ADC2D;
+	ADMUX |= 1 << MUX1;
+	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+	ADCSRA |= (1 << ADEN);
 }
 
 int adc_read(char a)
 {
 	ADMUX = a;
 	int dig;
-	ADCSRA |= 1<<ADSC;
-	while(ADCSRA & (1<<ADSC));
+	ADCSRA |= 1 << ADSC;
+	while (ADCSRA & (1 << ADSC)) ;
 	dig = ADCW;
 	return dig;
 }
 
-
 void uart_send_byte(unsigned char data)
 {
 	unsigned char i;
-	TIMSK0 &= ~(1<<OCIE0A);
-	TIFR0 |= 1<<OCF0A;
+	TIMSK0 &= ~(1 << OCIE0A);
+	TIFR0 |= 1 << OCF0A;
 	TCNT0 = 0;
-	TIMSK0 |= 1<OCIE0A;
-	PORTB &= ~(1<<PB3);
-	while(!(TIFR0 & (1<<OCF0A)));
-	TIFR0 |= 1<<OCF0A;
-	for(i=0;i<8;i++) {
-			if(data & 1)
-					PORTB |= 1<<PB3;
-			else 
-					PORTB &= ~(1<<PB3);
-			data >>= 1;
-			while(!(TIFR0 & (1<<OCF0A)));
-			TIFR0 |= 1<<OCF0A;
+	TIMSK0 |= 1 < OCIE0A;
+	PORTB &= ~(1 << PB3);
+	while (!(TIFR0 & (1 << OCF0A))) ;
+	TIFR0 |= 1 << OCF0A;
+	for (i = 0; i < 8; i++) {
+		if (data & 1)
+			PORTB |= 1 << PB3;
+		else
+			PORTB &= ~(1 << PB3);
+		data >>= 1;
+		while (!(TIFR0 & (1 << OCF0A))) ;
+		TIFR0 |= 1 << OCF0A;
 	}
-	PORTB |= 1<<PB3;
-	while(!(TIFR0 & (1<<OCF0A)));
-	TIFR0 |= 1<<OCF0A;
-}
-	
-print(char *p) {
-		while(*p){
-				uart_send_byte(*p++);
-		}
+	PORTB |= 1 << PB3;
+	while (!(TIFR0 & (1 << OCF0A))) ;
+	TIFR0 |= 1 << OCF0A;
 }
 
+print(char *p)
+{
+	while (*p) {
+		uart_send_byte(*p++);
+	}
+}
 
 void uart_print_num(int i)
 {
@@ -76,41 +101,40 @@ void uart_print_num(int i)
 		uart_send_byte(b[count] + '0');
 }
 
-
 main()
 {
-	int x,y;
+	int x, y;
 
-	DDRB|= 1<<PB3;		
-	PORTB |= 1<<PB3;
-	TCCR0A |= 1<<WGM01; // compare mode
-	TCCR0B |= (1<<CS00); //no prescaler
+	DDRB |= 1 << PB3;
+	PORTB |= 1 << PB3;
+	TCCR0A |= 1 << WGM01;	// compare mode
+	TCCR0B |= (1 << CS00);	//no prescaler
 	adc_init();
-	OCR0A = 83;  //for baudrate 57600
-	char a=0;
-	while(1) {
-			DDRB |= (1<<PB2)|(1<<PB0);
-			DDRB &= ~((1<<PB4)|(1<<PB1));//ADC 2
-			PORTB &= ~((1<<PB4)|(1<<PB1));
-			PORTB |= 1<<PB2;
-			PORTB &= ~(1<<PB0);
-	//		_delay_ms(10);
-			x=adc_read(2);
-			
-			DDRB |= (1<<PB4)|(1<<PB1);
-			DDRB &= ~((1<<PB2)|(1<<PB0));//ADC 1
-			PORTB &= ~((1<<PB2)|(1<<PB0));
-			PORTB |= 1<<PB1;
-			PORTB &= ~(1<<PB4);
-	//		_delay_ms(10);
-			y=adc_read(1);
-			if(x<900 && y<900) {
-				print("X: ");
-				uart_print_num(x);
-				uart_send_byte(' ');
-				print("Y: ");
-				uart_print_num(y);
-				uart_send_byte('\n');
-			}
+	OCR0A = 83;		//for baudrate 57600
+	char a = 0;
+	while (1) {
+		DDRB |= (1 << PB2) | (1 << PB0);
+		DDRB &= ~((1 << PB4) | (1 << PB1));	//ADC 2
+		PORTB &= ~((1 << PB4) | (1 << PB1));
+		PORTB |= 1 << PB2;
+		PORTB &= ~(1 << PB0);
+		//              _delay_ms(10);
+		x = adc_read(2);
+
+		DDRB |= (1 << PB4) | (1 << PB1);
+		DDRB &= ~((1 << PB2) | (1 << PB0));	//ADC 1
+		PORTB &= ~((1 << PB2) | (1 << PB0));
+		PORTB |= 1 << PB1;
+		PORTB &= ~(1 << PB4);
+		//              _delay_ms(10);
+		y = adc_read(1);
+		if (x < 900 && y < 900) {
+			print("X: ");
+			uart_print_num(x);
+			uart_send_byte(' ');
+			print("Y: ");
+			uart_print_num(y);
+			uart_send_byte('\n');
+		}
 	}
 }
